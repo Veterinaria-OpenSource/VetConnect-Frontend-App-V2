3e5452edfd3e1bb709b5d052a9503d382d22db3a
fabriciodev26 <youngdeveloper26@gmail.com>
2024-05-20 19:38:38 -0500

    style: update styles generals and responsive design

--- src/app/user-pet/components/google-maps-details/google-maps-details.component.ts
+++ src/app/user-pet/components/google-maps-details/google-maps-details.component.ts
@@ -1,12 +1,11 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-google-maps-details',
   templateUrl: './google-maps-details.component.html',
-  styleUrl: './google-maps-details.component.css'
+  styleUrl: './google-maps-details.component.css',
 })
 export class GoogleMapsDetailsComponent {
-
   lat: number = 51.678418;
   lng: number = 7.809007;
   zoom: number = 16;
@@ -14,18 +13,14 @@ export class GoogleMapsDetailsComponent {
   height: string = '400px';
   width: string = '100%';
 
-
   center: google.maps.LatLngLiteral = { lat: this.lat, lng: this.lng };
+  constructor(private elRef: ElementRef) {}
 
-  // lo di todo para hacerlo responsive
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    if (event.target.innerWidth <= 768) { // 768px is a common breakpoint for mobile devices
-      this.width = '200px';
-    } else {
-      this.width = '400px';
+  ngAfterViewInit() {
+    const mapContainer =
+      this.elRef.nativeElement.querySelector('.map-container');
+    if (mapContainer) {
+      mapContainer.style.width = '100%';
     }
   }
-  
-
 }
